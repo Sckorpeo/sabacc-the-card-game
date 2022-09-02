3948120e7c8848cc1f2e0af0565c4e85f35962ec
Kieran B <kieranbru.web@gmail.com>
2022-09-01 21:31:34 -0400

    Got sockets to connect

--- server/index.js
+++ server/index.js
@@ -3,22 +3,12 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
 
 // static middleware
 app.use(cors());
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
-
-io.on('connection', (socket) => {
-    console.log('a user connected.');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -26,12 +16,20 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, async () => {
+const server = app.listen(port, async () => {
     try {
         console.log(`listening on port ${port}`);
     } catch (ex) {
         console.log(ex);
     }
+});
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected.');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 module.exports = app;
